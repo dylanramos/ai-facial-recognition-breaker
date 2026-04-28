8b6a3692f2b2da57c73b54b3839e4130660e6efa
Dylan Ramos <github.5mwg9@simplelogin.com>
2026-04-28 15:35:33 +0200

    Add black frames broadcast

--- src/aifrb/commands/broadcast.py
+++ src/aifrb/commands/broadcast.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Annotated
 
 import cv2
+import numpy as np
 import pyvirtualcam
 import typer
 from pyvirtualcam import PixelFormat
@@ -12,45 +13,57 @@ app = typer.Typer()
 
 @app.command(rich_help_panel="Camera Commands", no_args_is_help=True)
 def broadcast(
-    video: Annotated[Path, typer.Argument(help="Path to the video file.")]
+    video: Annotated[Path, typer.Option("--video", "-v",help="Path to the video file.")] = None,
+    black_frame: Annotated[bool, typer.Option("--black-frame", "-b", help="Broadcast a black frame instead of a video.")] = False
 ):
     """
-    Broadcast a video file to a virtual camera.
+    Broadcast a black frame or a video file to a virtual camera.
     """
     if platform.system() == "Linux":
         # Reload the v4l2loopback module
         subprocess.run(["sudo", "modprobe", "-r", "v4l2loopback"])
         # Create the virtual camera
         subprocess.run(["sudo", "modprobe", "v4l2loopback", "devices=1", "video_nr=2", "card_label=\"Virtual Cam\"", "exclusive_caps=1"])
+        # Grant current user access to the device without requiring video group membership
+        subprocess.run(["sudo", "chmod", "666", "/dev/video2"])
 
-    # Source: https://github.com/letmaik/pyvirtualcam/blob/main/examples/video.py
-    video = cv2.VideoCapture(video)
-    if not video.isOpened():
-        raise ValueError("Failed to open video file")
-    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = video.get(cv2.CAP_PROP_FPS)
     device = None # Use default device
 
-    with pyvirtualcam.Camera(width, height, fps, fmt=PixelFormat.BGR,device=device, print_fps=fps) as cam:
-        print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
-        count = 0
-        while True:
-            # Restart video on last frame
-            if count == length:
-                count = 0
-                video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    if video is not None:
+        # Source: https://github.com/letmaik/pyvirtualcam/blob/main/examples/video.py
+        video = cv2.VideoCapture(video)
+        if not video.isOpened():
+            raise ValueError("Failed to open video file")
+        length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
 
-            # Read video frame
-            ret, frame = video.read()
-            if not ret:
-                raise RuntimeError('Error fetching frame')
+        with pyvirtualcam.Camera(width, height, fps, fmt=PixelFormat.BGR,device=device, print_fps=fps) as cam:
+            print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
+            count = 0
+            while True:
+                # Restart video on last frame
+                if count == length:
+                    count = 0
+                    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-            # Send to virtual cam
-            cam.send(frame)
+                # Read video frame
+                ret, frame = video.read()
+                if not ret:
+                    raise RuntimeError('Error fetching frame')
 
-            # Wait until it's time for the next frame
-            cam.sleep_until_next_frame()
+                # Send to virtual cam
+                cam.send(frame)
 
-            count += 1
+                # Wait until it's time for the next frame
+                cam.sleep_until_next_frame()
+
+                count += 1
+    elif black_frame:
+        with pyvirtualcam.Camera(width=1280, height=720, fps=20) as cam:
+            print(f'Using virtual camera: {cam.device}')
+            frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
+            while True:
+                cam.send(frame)
+                cam.sleep_until_next_frame()
