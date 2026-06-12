fb355b161136de9518fc3e2435ba8c0707e5f5d2
Dylan Ramos <github.5mwg9@simplelogin.com>
2026-06-12 10:46:33 +0200

    Rename command help

--- src/aifrb/commands/remaining_credits.py
+++ src/aifrb/commands/remaining_credits.py
@@ -5,8 +5,8 @@ from aifrb.api.kieai.utils import get_remaining_credits
 app = typer.Typer()
 
 
-@app.command(rich_help_panel="Kie Account Commands")
+@app.command(rich_help_panel="KIE AI Account Commands")
 def remaining_credits():
-    """Get the current credit balance available in the Kie account."""
+    """Get the current credit balance available in the KIE AI account."""
     credits = get_remaining_credits()
     print(f"Remaining credits: {credits}")
